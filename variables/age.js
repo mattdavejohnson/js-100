819d82a34285bf0f46020b869de8702a7f223864
Matthew Johnson <matthew.dave.johnson@gmail.com>
2021-09-09 00:34:55 -0700

    Completed assignments for variables chapter

---
+++ variables/age.js
@@ -0,0 +1,10 @@
+// Write a program that includes someone's age and then calculates
+// and reports the future age in 10, 20, 30 and 40 years.
+
+let age = 20;
+
+console.log(`You are ${age} years old.`);
+console.log(`In 10 years, you will be ${age + 10} years old.`);
+console.log(`In 10 years, you will be ${age + 20} years old.`);
+console.log(`In 10 years, you will be ${age + 30} years old.`);
+console.log(`In 10 years, you will be ${age + 40} years old.`);
---
+++ variables/greeter.js
@@ -0,0 +1,8 @@
+// Write a program that greets 'Victor' three times using a variable
+// instead of a hard code string value.
+
+let name = 'Victor';
+
+console.log(`Good Morning, ${name}`);
+console.log(`Good Afternoon, ${name}`);
+console.log(`Good Evening, ${name}`);
